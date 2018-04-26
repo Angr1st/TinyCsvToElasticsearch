577bcc947362cf9a7730e6db0ed4752584beabf1
Torben Clasen <torbenclasen@gmail.com>
2018-04-26 18:15:11 +0200

    Add project files.

---
+++ TinyCSVToES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyCSVToES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
