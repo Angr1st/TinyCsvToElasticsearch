6c1668affd7d2502432f39ba96586de2da34274d
Torben Clasen <torbenclasen@gmail.com>
2018-06-28 22:43:53 +0200

    restructured the classes to bring them all to top level

--- CSVToESLib/ElasticsearchClient.cs
+++ CSVToESLib/ElasticsearchClient.cs
@@ -14,32 +14,33 @@ namespace CSVToESLib
             return await ElasticLowLevelClient.BulkAsync<StringResponse>(PostData.MultiJson(results.Where(result => result.IsValid).Select(result => new Object[] { new Index(result.Result, "1") }).Aggregate((newValue, oldValue) => oldValue.Concat(newValue).ToArray())));
         }
 
-        public class Index
+
+
+    }
+
+    class Index
+    {
+        public InnerIndex InnerIndexField;
+        public Person Person;
+
+        public Index(Person person, string id)
         {
-
-            public Index(Person person, string id)
-            {
-                Person = person;
-                InnerIndexField = new InnerIndex(id: id);
-            }
-
-            public class InnerIndex
-            {
-                public string _index;
-                public string _type;
-                public string _id;
-
-                public InnerIndex(string index = "persons", string type = "person", string id = "1")
-                {
-                    _index = index;
-                    _type = type;
-                    _id = id;
-                }
-            }
-
-            public InnerIndex InnerIndexField;
-            public Person Person;
+            Person = person;
+            InnerIndexField = new InnerIndex(id: id);
         }
+    }
 
+    class InnerIndex
+    {
+        public string _index;
+        public string _type;
+        public string _id;
+
+        public InnerIndex(string index = "persons", string type = "person", string id = "1")
+        {
+            _index = index;
+            _type = type;
+            _id = id;
+        }
     }
 }
