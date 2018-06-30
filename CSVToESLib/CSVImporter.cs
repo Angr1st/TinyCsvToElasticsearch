b2ec4b7480877208805737ee0605555cbdaacae0
Torben Clasen <torbenclasen@gmail.com>
2018-07-01 00:27:51 +0200

    Started working on generating IL

--- CSVToESLib/CSVImporter.cs
+++ CSVToESLib/CSVImporter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Reflection;
 
 namespace CSVToESLib
 {
@@ -15,5 +17,20 @@ namespace CSVToESLib
             var result = await ElasticsearchClient.BulkInsert(CSVClient.Parse(filePath));
             return result.Success;
         }
+
+        public void CreateIL()
+        {
+            AssemblyName assemblyName = new AssemblyName(NameGenerator());
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect);
+
+            var module = assembly.DefineDynamicModule("dynamicModule");
+
+            var personType = module.DefineType("Person", TypeAttributes.Public);
+
+            personType.DefineField
+
+        }
+
+        public string NameGenerator() => new Guid().ToString();
     }
 }
