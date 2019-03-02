e28f6db8c461a1c57dccc143cce1285598a9da27
Torben Clasen <torbenclasen@gmail.com>
2019-03-02 02:28:49 +0100

    Made the version settable at runtime

--- CSVToESLib/Template/CsvImporter.cs
+++ CSVToESLib/Template/CsvImporter.cs
@@ -5,12 +5,11 @@ namespace CSVToESLib.Template
 {
     class CsvImporter : ICsvImporter
     {
-        CSVClient CSVClient = new CSVClient();
-
         public async Task<bool> ImportCsv(ConnectionConfiguration connection, string filePath, int version)
         {
+            var CSVClient = new CsvClient();
             var ElasticsearClient = new ElasticsearchClient(connection);
-            var result = await ElasticsearClient.BulkInsert(CSVClient.Parse(filePath));
+            var result = await ElasticsearClient.BulkInsert(CSVClient.Parse(filePath),version);
             return result.Success;
         }
     }
