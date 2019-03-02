1a41e97484d645eff4ea64b26aa85832b670b0ed
Torben Clasen <torbenclasen@gmail.com>
2019-03-02 02:37:29 +0100

    added First Test

---
+++ CSVToESLib.Tests/CsvImporterCreatorTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using CSVToESLib;
+
+namespace CSVToESLib.Tests
+{
+    public class CsvImporterGeneratorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string[] nameArray = new string[] { "SystemThreadingTasks", "ElasticsearchNet", "SystemK", "SystemLinq", "TinyCsvParserMapping", "TinyCsvParserK", "CSVToESLibK", "SystemConsole", "Person", "Test" };
+            string[] newArray = new string[] { "SystemThreadingTasks1", "ElasticsearchNet", "SystemK", "SystemLinq", "TinyCsvParserMapping", "TinyCsvParserK", "CSVToESLibK", "SystemConsole", "Person", "Test" };
+
+            var test = CsvImporterGenerator.CreateBulkPriceImporterType(nameArray);
+            var test2 = CsvImporterGenerator.CreateBulkPriceImporterType(newArray);
+
+            Assert.NotEqual(test, test2);
+        }
+    }
+}
