1b4d44389c9a8380c6f638d0a26ce3e99fd759d0
Torben Clasen <torbenclasen@gmail.com>
2019-09-15 21:46:44 +0200

    This creates the ability to await the oberserable

---
+++ CSVToESLib/ElasticsearchConnection.cs
@@ -0,0 +1,39 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVToESLib
+{
+    public class ElasticsearchConnection
+    {
+        public int RecordCount { get; private set; }
+
+        public BulkAllObserver BulkAllObserver
+        {
+            get => _bulkAllObserver;
+            private set
+            {
+                _bulkAllObserver = value;
+            }
+        }
+
+        private readonly TaskCompletionSource<bool> TaskCompletionSource = new TaskCompletionSource<bool>();
+
+        private BulkAllObserver _bulkAllObserver = null;
+
+        public ElasticsearchConnection()
+        {
+            BulkAllObserver = new BulkAllObserver(IncrementRecordCount, SetException, SetToComplete);
+        }
+
+        public async Task<bool> WaitForCompletion() => await TaskCompletionSource.Task;
+
+        private void IncrementRecordCount(IBulkAllResponse response) => RecordCount++;
+
+        private void SetException(Exception e) => TaskCompletionSource.SetException(e);
+
+        private void SetToComplete() => TaskCompletionSource.SetResult(true);
+    }
+}
