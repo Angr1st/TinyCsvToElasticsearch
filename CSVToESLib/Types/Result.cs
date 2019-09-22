01347213d7d55b6aaa02059a33c27d9f4f45d12b
Torben Clasen <torbenclasen@gmail.com>
2019-09-23 00:20:38 +0200

    Improved Result

--- CSVToESLib/Types/Result.cs
+++ CSVToESLib/Types/Result.cs
@@ -12,7 +12,7 @@ namespace CSVToESLib.Types
 
         public bool IsSuccess
         {
-            get => Left != null ? true : false;
+            get => IsSucceded(Left,Right);
         }
 
         public Result(T left)
@@ -24,5 +24,22 @@ namespace CSVToESLib.Types
         {
             Right = right;
         }
+
+        private static bool IsSucceded(T left, U error)
+        {
+            return !CheckDefaultOrNull(left) && CheckDefaultOrNull(error);
+        }
+
+        private static bool CheckDefaultOrNull<Y>(Y variable)
+        {
+            if (IsNullable(typeof(Y)))
+            {
+                return variable == null;
+            }
+
+            return default(Y).Equals(variable);
+        }
+
+        private static bool IsNullable(Type type) => Nullable.GetUnderlyingType(type) != null || type.IsClass || type.IsInterface;
     }
 }
